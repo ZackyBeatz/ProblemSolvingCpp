ad4d89a998ecc5ab14e131946a0b97f189ecb2ab
ZackyBeatz <zacky89@hotmail.com>
2015-09-27 22:44:00 +0200

    simple write code correctly

---
+++ exercise35.cpp
@@ -0,0 +1,26 @@
+//
+//  exercise35.cpp
+//  SelfTestExercise
+//
+//  Created by Zakaria Ahmed on 27/09/15.
+//  Copyright © 2015 ProblemSolvingWithCpp. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int x = 0;
+    
+    if (x < 0) {
+        x = 7; cout << "x is now positive.";}
+    
+    else
+    {
+        x = - 7; cout << "x is now negative.";
+    }
+    
+    return 0;
+}
