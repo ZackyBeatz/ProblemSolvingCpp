7c5364830ea3f42420f1dea8979baa6887277f45
ZackyBeatz <zacky89@hotmail.com>
2015-09-27 22:46:15 +0200

    Exercise35 done

--- exercise35.cpp
+++ exercise35.cpp
@@ -6,6 +6,12 @@
 //  Copyright © 2015 ProblemSolvingWithCpp. All rights reserved.
 //
 
+/* 
+ exercise 35. 
+ The following if-else statement will compile and run without any problems. However, it is not laid out in a way that is consistent with the other if-else statements we have used in our programs. Rewrite it so that the layout (indenting and line breaks) matches the style we used in this chapter.
+ if (x < 0) {x = 7; cout << "x is now positive.";} else {x = - 7; cout << "x is now negative.";}
+ */
+
 #include <stdio.h>
 #include <iostream>
 using namespace std;
